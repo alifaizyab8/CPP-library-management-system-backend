5877f0da1ab03bb05ff226a6f05201b337075f16
alifaizyab8 <alifaizyab8@gmail.com>
2026-01-25 00:08:06 +0500

    Added additional  loan variables and getters

--- src/domain/Loan.h
+++ src/domain/Loan.h
@@ -1,18 +1,26 @@
 #pragma once
 #include <string>
-#include<utility>
+#include <utility>
+#include <optional>
 
 class Loan
 {
 private:
+    std::string id; // Unique ID will be generated for the loan transaction
     std::string isbn;
     std::string userID;
-
+    std::string issueDate;                 // Strictly follow format YYYY-MM-DD
+    std::optional<std::string> returnDate; // Empty if not returned yet
 public:
     ~Loan() = default;
-    Loan(std :: string isbn, std:: string userID);
+    Loan(std ::string id, std::string isbn, std ::string userID, std::string issueDate);
 
-    const std:: string& getISBN() const;
-    const std:: string& getUserID() const;
-    
+    void markReturned(std::string date) const;
+    bool isActive() const; // Returns true if return date is empty meaning book is still issued
+
+    const std::string &getID() const;
+    const std::string &getISBN() const;
+    const std::string &getUserID() const;
+    const std::string &getIssueDate() const;
+    std ::optional<std::string> getReturnDate() const;
 };
