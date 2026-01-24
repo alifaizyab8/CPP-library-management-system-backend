bd0c2a5f0a338ccd723b14554e8f4a49c226fd17
alifaizyab8 <alifaizyab8@gmail.com>
2026-01-24 23:57:01 +0500

    Implemented Location functionality for Book Object and added location getter

--- src/domain/Book.h
+++ src/domain/Book.h
@@ -1,18 +1,29 @@
 #pragma once
 #include <string>
 
+struct bookLocation
+{
+    std::string aisle;
+    std::string shelf;
+    std::string bin;
+};
+
 class Book
 {
 
 private:
+    // Considering that 1 ISBN = 1 BOOK for simple implementation
     std ::string title;
     std ::string author;
     std ::string isbn;
+    bookLocation locator;
 
 public:
     ~Book() = default;
-    Book(std ::string title, std ::string author, std ::string isbn);
+    Book(std ::string title, std ::string author, std ::string isbn, bookLocation locator);
+    // Getters
     const std ::string &getTitle() const;
     const std ::string &getAuthor() const;
     const std ::string &getISBN() const;
+    const std::string &getLocation() const;
 };
